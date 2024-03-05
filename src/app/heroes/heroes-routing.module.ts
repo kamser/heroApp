9dd6ecd52bf8df7b309da0327fcadfe0fa43b16c
kamser <keylor.morataya@gmail.com>
2024-03-04 20:56:23 -0800

    added the default route for the heroes page which going to address the loyout-component page

--- src/app/heroes/heroes-routing.module.ts
+++ src/app/heroes/heroes-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    //This page going to be display when: localhost:4200/heroes
+    path: '',
+    component: LayoutPageComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
