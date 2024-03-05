744eab751eb3977a999dbe21d30aa9dc613e7938
kamser <keylor.morataya@gmail.com>
2024-03-04 20:48:52 -0800

    Added a page for the shared module and updated the shared.module with the new component and took off the commondModule

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Error404PageComponent } from './pages/error-404-page/error-404-page.component';
 
 
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [
+    Error404PageComponent
+  ],
+  exports: [Error404PageComponent]
 })
 export class SharedModule { }
