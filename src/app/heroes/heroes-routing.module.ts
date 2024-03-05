3a096c0bf45c91e1b4e394149021ba2eb331228a
kamser <keylor.morataya@gmail.com>
2024-03-04 21:09:56 -0800

    Added the first child route for heroes which redirect to new-component. Modified the heroes-routing.module

--- src/app/heroes/heroes-routing.module.ts
+++ src/app/heroes/heroes-routing.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
+import { NewPageComponent } from './pages/new-page/new-page.component';
 
 const routes: Routes = [
   {
     //This page going to be display when: localhost:4200/heroes
     path: '',
-    component: LayoutPageComponent
+    component: LayoutPageComponent,
+    children: [
+      {
+        path: 'new-hero',
+        component: NewPageComponent
+      }
+    ]
   }
 ];
 
