edafaea4fc5397d7f6f1d4a6ad1b720b34fe49b2
kamser <keylor.morataya@gmail.com>
2024-03-04 20:54:34 -0800

    added the default route for the auth page which going to address the loyout-component page

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,8 +1,15 @@
 import {NgModule} from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    //To access this is: localhost:4200/auth
+    path: '',
+    component: LayoutPageComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
