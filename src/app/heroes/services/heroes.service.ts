28a5d004edbebf38920885c972c5ab102a6ff7b7
kamser <keylor.morataya@gmail.com>
2024-03-06 20:57:45 -0800

    Added the getHeroes() method to get the heroes list, using an Oservable, the http object and the enviroment variable for dev

--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -1,10 +1,18 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { enviroments } from '../../../enviroments/enviroments';
+import { Observable } from 'rxjs';
+import { Hero } from '../interfaces/hero.interface';
 
 @Injectable({providedIn: 'root'})
 export class ServiceNameService {
-  constructor(private httpClient: HttpClient) { }
 
+  private baseUrl: string = enviroments.baseUrl;
 
+  constructor(private http: HttpClient) { }
+
+  getHeroes(): Observable<Hero[]>{
+    return this.http.get<Hero[]>(`${this.baseUrl}/heroes`);
+  }
 
 }
