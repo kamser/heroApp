2ac950d05be35a2a82127461cce79f6612c5edd7
kamser <keylor.morataya@gmail.com>
2024-03-06 21:20:10 -0800

    Set the hero property as a input parameter for the cardComponent so it could get the hero info from the list-pege and did a validation in the onInit for the cardComponent

--- src/app/heroes/components/card/card.component.ts
+++ src/app/heroes/components/card/card.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Hero } from '../../interfaces/hero.interface';
 
 @Component({
-  selector: 'app-card',
+  selector: 'heroes-hero-card',
   templateUrl: './card.component.html',
   styleUrl: './card.component.css'
 })
-export class CardComponent {
+export class CardComponent implements OnInit{
+  @Input()
+  public hero!: Hero;
 
+  ngOnInit(): void {
+    if(!this.hero) throw Error('Hero property is required');
+  }
 }
