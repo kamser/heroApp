38f7ed534d7550ba628965ee2517bb70fba04abf
kamser <keylor.morataya@gmail.com>
2024-03-06 20:51:32 -0800

    Added the base of the heroes services and imported the HttpClientModule on the app.module.ts so I can user the HttpClient in the entire app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -13,6 +14,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
     SharedModule,
     BrowserAnimationsModule
   ],
