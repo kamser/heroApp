8d6e000c7625f3ebcae220237d89a59a04c31ad8
kamser <keylor.morataya@gmail.com>
2024-03-07 22:25:11 -0800

    Created the the endpoint to get the hero information by id using observables and handling error in case the hero does not exist

--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { enviroments } from '../../../enviroments/enviroments';
-import { Observable } from 'rxjs';
+import { Observable, catchError, of } from 'rxjs';
 import { Hero } from '../interfaces/hero.interface';
 
 @Injectable({providedIn: 'root'})
@@ -13,6 +13,13 @@ export class HeroesService {
 
   getHeroes(): Observable<Hero[]>{
     return this.http.get<Hero[]>(`${this.baseUrl}/heroes`);
+  }
+
+  getHeroById(heroId: string): Observable<Hero | undefined>{
+    return this.http.get<Hero>(`${this.baseUrl}/heroes/${heroId}`)
+      .pipe(
+        catchError(error => of(undefined))
+      );
   }
 
 }
