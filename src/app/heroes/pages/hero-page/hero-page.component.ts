2b3e3ddde98ff0de607e87284f4da6940053921f
kamser <keylor.morataya@gmail.com>
2024-03-07 22:40:31 -0800

    Inyected the heroService to the hero-page and also inyected the activateRoute and the router to performed actions with the navigation and the url and lastly, get it the hero data in the onInit for the hero-page component

--- src/app/heroes/pages/hero-page/hero-page.component.ts
+++ src/app/heroes/pages/hero-page/hero-page.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HeroesService } from '../../services/heroes.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { switchMap } from 'rxjs';
+import { Hero } from '../../interfaces/hero.interface';
 
 @Component({
   selector: 'app-hero-page',
   templateUrl: './hero-page.component.html',
   styleUrl: './hero-page.component.css'
 })
-export class HeroPageComponent {
+export class HeroPageComponent implements OnInit{
+  public hero?: Hero;
 
+  //the activatedRout is to have access to the params that came in the URL, so to have access to the URL.
+  //The router is use to redirect the use to other page progrmatically in case no hero were found.
+  constructor(private heroesService: HeroesService, private activatedRoute: ActivatedRoute, private router: Router){}
+
+  //this is use to get the data related to the sevice as soon as this component start.
+  ngOnInit(): void {
+    this.activatedRoute.params
+      .pipe(
+        switchMap(({id}) => this.heroesService.getHeroById(id)),
+      ).subscribe( hero => {
+        if(!hero) return this.router.navigate(['/heroes/list']);
+
+        this.hero = hero;
+        console.log(hero);
+        return;
+      });
+  }
 }
