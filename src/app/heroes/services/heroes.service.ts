47592c05a28ccda8ae6ae83bd7360ae63128bc57
kamser <keylor.morataya@gmail.com>
2024-03-08 17:48:28 -0800

    Added the addHero, updateHero and deleteHero to the hero-service

--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { enviroments } from '../../../enviroments/enviroments';
-import { Observable, catchError, of } from 'rxjs';
+import { Observable, catchError, map, of } from 'rxjs';
 import { Hero } from '../interfaces/hero.interface';
 
 @Injectable({providedIn: 'root'})
@@ -24,6 +24,25 @@ export class HeroesService {
 
   getSuggestions(query: string): Observable<Hero[]>{
     return this.http.get<Hero[]>(`${this.baseUrl}/heroes?q=${query}&_limit=6`);
+  }
+
+  addHero(hero: Hero): Observable<Hero>{
+    return this.http.post<Hero>(`${this.baseUrl}/heroes`, hero); //Second parameter are the values that going to be use to create the row on the database.
+  }
+
+  updateHero(hero: Hero): Observable<Hero>{
+    if(!hero){
+      throw Error('Hero id is required');
+    }
+    return this.http.patch<Hero>(`${this.baseUrl}/heroes/${hero.id}`, hero); //Second parameter are the values that going to be use to create the row on the database.
+  }
+
+  deleteHero(id: string): Observable<boolean>{
+    return this.http.delete(`${this.baseUrl}/heroes/${id}`)
+            .pipe(
+              catchError(err => of(false)),
+              map(resp => true), //This transform the response to a value that we need, whatever it return, it going to be a observable by default
+            );
   }
 
 }
