beed21484bcdbe40bffa23f654ee269e0cfd18e5
kamser <keylor.morataya@gmail.com>
2024-03-08 22:18:31 -0800

    Implemented the ngOnInit to populate the hero data automatically in the reactive form when the user wants to edit it using the reset() funtion to do that

--- src/app/heroes/pages/new-page/new-page.component.ts
+++ src/app/heroes/pages/new-page/new-page.component.ts
@@ -1,14 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { Hero, Publisher } from '../../interfaces/hero.interface';
 import { HeroesService } from '../../services/heroes.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { switchMap } from 'rxjs';
+
 
 @Component({
   selector: 'app-new-page',
   templateUrl: './new-page.component.html',
   styleUrl: './new-page.component.css'
 })
-export class NewPageComponent {
+export class NewPageComponent implements OnInit{
   /**To create a Reactive form, I need to create a general FormGroup and inner that FormGroup, create
    * the the FormControl whom represents the values or atributes that I want to handle
    * with my reactive form, so normally, we put all the inputs we have
@@ -29,7 +32,22 @@ export class NewPageComponent {
     {id: 'Marverl comics', desc: 'Marvel-Studios'},
   ];
 
-  constructor(private heroService: HeroesService){}
+  constructor(private heroService: HeroesService, private activatedRoute: ActivatedRoute, private router: Router){}
+
+
+  ngOnInit(): void {
+    if(!this.router.url.includes('edit')) return;
+
+    this.activatedRoute.params
+          .pipe(
+            switchMap(({id}) => this.heroService.getHeroById(id)),
+          ).subscribe( hero => {
+            if(!hero) return this.router.navigateByUrl('/');
+
+            this.reactiveHeroForm.reset(hero); //This helps to assign the values to the reactive form automatically with the hero information that arrives.
+            return;
+          })
+  }
 
   get currentHeroFromForm(): Hero{
     const hero = this.reactiveHeroForm.value as Hero;
